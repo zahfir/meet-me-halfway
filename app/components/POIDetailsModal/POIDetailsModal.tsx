71b15ab0dc0bf8158d51ba93b130a0fb88b46d61
zahfir <zafy2001@gmail.com>
2024-10-31 17:23:02 -0400

    more details button styling

--- app/components/POIDetailsModal/POIDetailsModal.tsx
+++ app/components/POIDetailsModal/POIDetailsModal.tsx
@@ -14,12 +14,10 @@ const POIDetailsModal: React.FC = () => {
   const { isOpen, closingTime } = selectedPOI?.closingTimeToday() ?? {};
   const address = selectedPOI?.address();
   const website = selectedPOI?.website();
-
   const distanceFromUser = selectedPOI?.distanceFromUser()?.toFixed(1) ?? false;
 
-  // ICON
+  // ICON - Replace this with a short circuiting function
   const iconSize = 40;
-  // Replace this with a short circuiting function
   const IconComponent = category ? CategoryIconMap[category] : null;
   const RenderedIcon = IconComponent ? <IconComponent size={iconSize} /> : null;
 
@@ -30,7 +28,9 @@ const POIDetailsModal: React.FC = () => {
       } d-flex flex-column col-3 m-4 p-3
       bg-black text-white rounded-4`}
     >
+      {/* HEADER */}
       <div className="d-flex">
+        {/* ICON */}
         <div className="me-4">
           {category && (
             <div className="bg-primary p-2 d-inline-flex align-items-center justify-content-center rounded-circle">
@@ -57,6 +57,7 @@ const POIDetailsModal: React.FC = () => {
           </span>
         </div>
       </div>
+      {/* MIDDLE BANNER */}
       <div
         className="my-3 p-2 border border-dark rounded-3"
         style={{ backgroundColor: "#1f1f1f" }}
@@ -64,10 +65,15 @@ const POIDetailsModal: React.FC = () => {
         Misc Tags Go Here
       </div>
       <h5>{address}</h5>
+      {/* MORE DETAILS BUTTON */}
       {website && (
         <a href={website} target="_blank" rel="noopener noreferrer">
-          {/* TODO STYLE THIS BUTTON */}
-          <button>More Details</button>
+          <button
+            className="w-100 mt-3 p-3 border border-dark rounded-3"
+            style={{ backgroundColor: "#1f1f1f" }}
+          >
+            <h5>More Details</h5>
+          </button>
         </a>
       )}
     </div>
