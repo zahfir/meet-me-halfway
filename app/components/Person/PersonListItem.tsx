2e447f73cace124f0dfee4b5e93a40d0c008f0d1
zahfir <zafy2001@gmail.com>
2024-11-05 14:19:23 -0500

    More person address formatting

--- app/components/Person/PersonListItem.tsx
+++ app/components/Person/PersonListItem.tsx
@@ -14,10 +14,13 @@ interface PersonListItemProps {
 const PersonListItem: FC<PersonListItemProps> = ({ person }) => {
   const { removePerson } = useMapStore();
   const [isHover, setIsHover] = useState(false);
-  const addressLineOne =
-    person.address.formattedAddressLineOne || person.address.display_name;
-  const addressLineTwo = person.address.formattedAddressLineTwo || "";
 
+  const address = person.address;
+  const addressLineOne =
+    address.formattedAddressLineOne || address.display_name.split(",")[0];
+  const addressLineTwo = address.formattedAddressLineTwo;
+
+  // Callbacks for route animation
   const onMouseEnter = () => {
     if (isHover) return;
     setIsHover(true);
@@ -27,6 +30,7 @@ const PersonListItem: FC<PersonListItemProps> = ({ person }) => {
   const onMouseLeave = () => {
     setIsHover(false);
   };
+
   return (
     <li
       key={person.id}
@@ -42,9 +46,11 @@ const PersonListItem: FC<PersonListItemProps> = ({ person }) => {
             <CarIcon color={person.marker?._color} />
           </span>
           <div className="d-flex flex-column">
-            {/* Address */}
+            {/* ADDRESS */}
             <strong>{addressLineOne}</strong>
-            <label className="fw-light">{addressLineTwo}</label>
+            {addressLineTwo && (
+              <label className="fw-light">{addressLineTwo}</label>
+            )}
           </div>
         </div>
 
